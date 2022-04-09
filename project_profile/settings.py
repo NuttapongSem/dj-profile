e7d1e4677824a4715ecca33520a93c5897b47b73
nutaapong <nattapong.sem@gmail.com>
2022-04-10 00:45:24 +0700

    remove1

--- project_profile/settings.py
+++ project_profile/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,7 +84,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+DATABASES['default'] = dj_database_url.config(default='postgres://pclwmdxzoghmlj:0b7b77cb6d23ffad8bcae037af1cb874972dd17ff18f5f641cf0a9f336db0818@ec2-34-207-12-160.compute-1.amazonaws.com:5432/dq55fvh5ap36v')
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
