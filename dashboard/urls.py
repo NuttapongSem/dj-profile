f4656f1e75a8ec49720971759e40f77bacc354c0
nutaapong <nattapong.sem@gmail.com>
2022-04-09 23:07:03 +0700

    deploy project

---
+++ dashboard/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+    path('', views.index, name='dashboard'),
+    path('abouts/', views.abouts, name='abouts'),
+    path('delete_skill/<int:id>', views.deleteSkill, name='delete-skill')
+]
